7bf67514f0324d06aadf83111763a974340fa989
TRAN QUANG NAM <nam.fpu@gmail.com>
2024-11-06 23:21:28 +0900

    multiplication table.java

---
+++ multiplication table.java
@@ -0,0 +1,11 @@
+
+public class multiplication table {
+	public static void main(String[] args) {
+		for (int i = 2; i <= 9; i++) {
+			for (int j = 1; j <= 10; j++) {
+				System.out.println(i + "x" + j + " = " + i * j);
+			}
+			System.out.println("----");
+		}
+	}
+}
